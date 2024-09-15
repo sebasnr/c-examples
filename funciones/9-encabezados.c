41b95824daf94c6e663065946bdc76ba4d78c5f1
sebasnr <sebastian.nunezrojas@ucr.ac.cr>
2024-09-14 18:05:25 -0600

    Se incluye la funciÓn 5

--- funciones/9-encabezados.c
+++ funciones/9-encabezados.c
@@ -2,16 +2,18 @@
 #include "9-encabezados.h"
 
 int main() {
-
+    
     int v;
+    int f;
 
     // Llama a las 3 funciones
     funcion1();
     funcion2(5, 2.67);
     v = funcion3(10);
-
+    f = funcion5(10);
     // Imprime el valor que retornó funcion3()
     printf("v = %d\n", v);
+    printf("f = %d\n", f);
 
     return 0;
 
@@ -37,8 +39,8 @@ int funcion3(int x) {
 }
 
 int funcion5(int x) {
-    printf("en funcion5");
-int funcion4(int x) {
-    printf("En funcion4()");
-    return x;
+    printf("En funcion3()...\n");
+    int y = x + 5;
+    return y;
 }
+
